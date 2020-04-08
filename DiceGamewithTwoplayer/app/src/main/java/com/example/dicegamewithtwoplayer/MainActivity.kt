cc493af1aac39b6c954b79b88ddb5497637ec191
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-04-08 08:34:44 +0530

    07-april-2020 task2 added

--- DiceGamewithTwoplayer/app/src/main/java/com/example/dicegamewithtwoplayer/MainActivity.kt
+++ DiceGamewithTwoplayer/app/src/main/java/com/example/dicegamewithtwoplayer/MainActivity.kt
@@ -6,69 +6,69 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.dicegamewithtwoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var img1:ImageView
-    lateinit var img2:ImageView
     var r1:Int = 0
     var r2:Int = 0
-    lateinit var tv: TextView
+    lateinit var dataBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        img1 = findViewById(R.id.pdice1)
-        img2 = findViewById(R.id.pdice2)
-        tv=findViewById(R.id.result)
+        dataBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 
     fun roll(view: View) {
         r1 = (1..6).random()
         r2 = (1..6).random()
-        displayDice1(r1)
-        displayDice2(r2)
+        var d = MyData(r1,r2)
+        displayDice1(d.r1)
+        displayDice2(d.r2)
 
-        tv.text = r1.toString()+"----"+r2.toString()
+        dataBinding.result.text = d.r1.toString()+"----"+d.r2.toString()
     }
 
     private fun displayDice2(r2: Int) {
         when(r2) {
-            1 -> img2.setImageResource(R.drawable.dice_1)
-            2 -> img2.setImageResource(R.drawable.dice_2)
-            3 -> img2.setImageResource(R.drawable.dice_3)
-            4 -> img2.setImageResource(R.drawable.dice_4)
-            5 -> img2.setImageResource(R.drawable.dice_5)
-            6 -> img2.setImageResource(R.drawable.dice_6)
+            1 -> dataBinding.pdice2.setImageResource(R.drawable.dice_1)
+            2 -> dataBinding.pdice2.setImageResource(R.drawable.dice_2)
+            3 -> dataBinding.pdice2.setImageResource(R.drawable.dice_3)
+            4 -> dataBinding.pdice2.setImageResource(R.drawable.dice_4)
+            5 -> dataBinding.pdice2.setImageResource(R.drawable.dice_5)
+            6 -> dataBinding.pdice2.setImageResource(R.drawable.dice_6)
             else -> {
-                img2.setImageResource(R.drawable.empty_dice)
+                dataBinding.pdice2.setImageResource(R.drawable.empty_dice)
             }
         }
     }
 
     private fun displayDice1(r1: Int) {
         when(r1){
-            1 -> img1.setImageResource(R.drawable.dice_1)
-            2 -> img1.setImageResource(R.drawable.dice_2)
-            3 -> img1.setImageResource(R.drawable.dice_3)
-            4 -> img1.setImageResource(R.drawable.dice_4)
-            5 -> img1.setImageResource(R.drawable.dice_5)
-            6 -> img1.setImageResource(R.drawable.dice_6)
+            1 -> dataBinding.pdice1.setImageResource(R.drawable.dice_1)
+            2 -> dataBinding.pdice1.setImageResource(R.drawable.dice_2)
+            3 -> dataBinding.pdice1.setImageResource(R.drawable.dice_3)
+            4 -> dataBinding.pdice1.setImageResource(R.drawable.dice_4)
+            5 -> dataBinding.pdice1.setImageResource(R.drawable.dice_5)
+            6 -> dataBinding.pdice1.setImageResource(R.drawable.dice_6)
             else -> {
-                img1.setImageResource(R.drawable.empty_dice)
+                dataBinding.pdice1.setImageResource(R.drawable.empty_dice)
             }
         }
     }
 
     fun upCount(view: View) {
         if((r1 == 0) and (r2 == 0)){
-            img1.setImageResource(R.drawable.dice_1)
-            img2.setImageResource(R.drawable.dice_1)
+            dataBinding.pdice1.setImageResource(R.drawable.dice_1)
+            dataBinding.pdice2.setImageResource(R.drawable.dice_1)
         }else if((r1<6) and (r2<6)){
-            r1++
-            r2++
-            tv.text = r1.toString()+"----"+r2.toString()
-            displayDice1(r1)
-            displayDice2(r2)
+            var d = MyData(r1,r2)
+            d.r1++
+            d.r2++
+            dataBinding.result.text = d.r1.toString()+"----"+d.r2.toString()
+            displayDice1(d.r1)
+            displayDice2(d.r2)
         }
 
 
